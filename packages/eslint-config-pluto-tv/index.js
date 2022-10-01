8d10b98d051250a1c426811357870946a8fe589b
Morgan Guo <morgan.guo@pluto.tv>
2022-09-30 21:51:32 -0700

    style: prettier with eslint

--- packages/eslint-config-pluto-tv/index.js
+++ packages/eslint-config-pluto-tv/index.js
@@ -1,13 +1,17 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaVersion: 'latest', // Allows the use of modern ECMAScript features
-        sourceType: 'module' // Allows for the use of imports
-    },
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'], // Uses the linting rules from @typescript-eslint/eslint-plugin
-    env: {
-        node: true // Enable Node.js global variables
-    },
-    ignorePatterns: ['build/', '*.js'],
-    root: true
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+      ecmaVersion: 'latest', // Allows the use of modern ECMAScript features
+      sourceType: 'module' // Allows for the use of imports
+  },
+  extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'prettier'
+  ], // Uses the linting rules from @typescript-eslint/eslint-plugin
+  env: {
+      node: true // Enable Node.js global variables
+  },
+  ignorePatterns: ['build/', '*.js'],
+  root: true
 };
