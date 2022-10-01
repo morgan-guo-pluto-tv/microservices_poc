8473d1b77ac3c5f5bdcc5a7fbe535860b0aa5759
Morgan Guo <morgan.guo@pluto.tv>
2022-10-01 00:00:03 -0700

    style: tsconfig package

--- packages/mService_fwUrlBuilder/src/app.ts
+++ packages/mService_fwUrlBuilder/src/app.ts
@@ -7,6 +7,3 @@ import { serviceImplementation, serviceName } from './services/fwUrlBuilder';
     await init(process.env.NATS_SERVER_URL as string, serviceName);
     await set(RequestSubject.ComposeFreewheelURL, serviceImplementation);
 })();
-
-export { RequestSubject };
-export { Request, Response } from './services/fwUrlBuilder';
